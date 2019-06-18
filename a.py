d1e9f8e1e4eb60810695aa2a487ed331e547e6ba
arman76 <armgham92@gmail.com>
2019-06-18 20:41:35 +0430

    Update a.py

--- a.py
+++ a.py
@@ -7,14 +7,16 @@ ml = []
 
 def main(bot):
     while True:
+        ml = []
+        with open('list.txt', 'r') as f:
+            for l in f.readlines():
+                ml.append(l.replace('\n', ''))
         for user in ml:
             scrp.main(bot, user.split('=')[0], {'username' : (user.split('=')[1]).split(':')[0], 'password' : (user.split('=')[1]).split(':')[1]})
             print(user.split('=')[0], {'username' : (user.split('=')[1]).split(':')[0], 'password' : (user.split('=')[1]).split(':')[1]})
             with open('list.txt', 'r') as f:
                 print(f.readlines())
             sleep(5)
-        ml = []
-        with open('list.txt', 'r') as f:
-            for l in f.readlines():
-                ml.append(l.replace('\n', ''))
+        
+        
 
