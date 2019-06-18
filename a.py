9f0f2764868af371ff80c2787a0e569ed0d20134
arman76 <armgham92@gmail.com>
2019-06-18 20:30:36 +0430

    Update a.py

--- a.py
+++ a.py
@@ -9,6 +9,7 @@ def main(bot):
     while True:
         for user in ml:
             scrp.main(bot, user.split('=')[0], {'username' : (user.split('=')[1]).split(':')[0], 'password' : (user.split('=')[1]).split(':')[1]})
+            print(user.split('=')[0], {'username' : (user.split('=')[1]).split(':')[0], 'password' : (user.split('=')[1]).split(':')[1]})
             sleep(5)
         with open('list.txt', 'r') as f:
             for l in f.readlines():
