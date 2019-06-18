5ae187c3e7cb0d3dd5e0d64ee9853311df42bbb4
arman76 <armgham92@gmail.com>
2019-06-18 20:29:10 +0430

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import _thread
 from time import sleep
 import scrp
 
@@ -7,16 +6,11 @@ ml = []
 
 
 def main(bot):
-    _thread.start_new_thread(update_list, (5,))
     while True:
         for user in ml:
             scrp.main(bot, user.split('=')[0], {'username' : (user.split('=')[1]).split(':')[0], 'password' : (user.split('=')[1]).split(':')[1]})
             sleep(5)
-
-
-def update_list(t):
-    while True:
         with open('list.txt', 'r') as f:
             for l in f.readlines():
                 ml.append(l.replace('\n', ''))
-        sleep(t)
+
