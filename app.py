93b0090e864328bfda923952890f7013b30de359
arman76 <armgham92@gmail.com>
2019-07-04 22:55:33 +0430

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import zipfile
 os.system('wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz')
 os.system('tar -zxvf geckodriver-v0.11.1-linux64.tar.gz')
 os.system('wget https://github.com/rafpyprog/Mobilenium/archive/master.zip')
-with zipfile.ZipFile("Mobilenium-master.zip","r") as zip_ref:
+with zipfile.ZipFile("master.zip","r") as zip_ref:
     zip_ref.extractall(".")
 
 os.system('wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2')
