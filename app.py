d31cf48b52376f0f02407c00f11ee8d483f69384
arman76 <armgham92@gmail.com>
2019-07-04 22:48:48 +0430

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, Rege
 import _thread
 import a
 import os
-#import zipfile
+import zipfile
 
 os.system('wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz')
 os.system('tar -zxvf geckodriver-v0.11.1-linux64.tar.gz')
 os.system('wget https://github.com/rafpyprog/Mobilenium/archive/master.zip')
-os.system('unzip Mobilenium-master.zip')
+with zipfile.ZipFile("Mobilenium-master.zip","r") as zip_ref:
+    zip_ref.extractall(".")
 
 os.system('wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2')
 os.system('tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2')
